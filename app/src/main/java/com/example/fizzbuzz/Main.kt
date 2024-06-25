53b963f0a6964b2feafb4375bbccdbf5bbf256c7
giaros <giacomor7@gmail.com>
2024-06-25 11:14:12 +0100

    Implementing FizzBuzz

--- app/src/main/java/com/example/fizzbuzz/Main.kt
+++ app/src/main/java/com/example/fizzbuzz/Main.kt
@@ -1,5 +1,20 @@
 package com.example.fizzbuzz
 
 fun main()  {
-    print("Hello world!")
+    var i = 1
+
+    do {
+        var result = ""
+        if (i % 3 == 0){
+            result += "Fizz"
+        }
+        if (i % 5 == 0){
+            result += "Buzz"
+        }
+        if (result == ""){
+            result += i.toString()
+        }
+        print(result + "\n")
+        i++
+    } while (i <= 100)
 }
