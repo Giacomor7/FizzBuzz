dafc21cf8956904c19026d6b09bb6cbd321065d5
giaros <giacomor7@gmail.com>
2024-06-25 12:03:01 +0100

    Adding Bang, Bong, Fezz and reversal

--- app/src/main/java/com/example/fizzbuzz/Main.kt
+++ app/src/main/java/com/example/fizzbuzz/Main.kt
@@ -4,17 +4,45 @@ fun main()  {
     var i = 1
 
     do {
-        var result = ""
+        var result = emptyArray<String>()
         if (i % 3 == 0){
             result += "Fizz"
         }
         if (i % 5 == 0){
             result += "Buzz"
         }
-        if (result == ""){
+        if (i % 7 == 0){
+            result += "Bang"
+        }
+        if (i % 11 == 0){
+            result = arrayOf("Bong") // opted for readability over performance: could refactor so previous if blocks do not evaluate in this case
+        }
+        if (i % 13 == 0){
+            var tempResult = emptyArray<String>()
+            var fezzAdded = false
+            for (word in result){
+                if (word[0] == 'B' && !fezzAdded){
+                    tempResult += "Fezz"
+                    fezzAdded = true
+                }
+                tempResult += word
+            }
+            if (!fezzAdded){
+                tempResult += "Fezz"
+            }
+            result = tempResult
+        }
+        if (i % 17 == 0){
+            result.reverse()
+        }
+        if (result.isEmpty()){
             result += i.toString()
         }
-        print(result + "\n")
+        var output = ""
+        for (word in result){
+            output += word
+        }
+        print(output + "\n") // result is now an array!
         i++
-    } while (i <= 100)
+    } while (i <= 300)
 }
